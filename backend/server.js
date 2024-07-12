69c4e15ddf2eb98a158289b91b6a84515df99597
aaron <aaronkornish.ak@gmail.com>
2024-07-12 14:51:01 -0700

    Initialize Express server with CORS middleware, movies route, and 404 error handling.

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import cors from 'cors'
+import movies from './api/movies.route.js'
+
+const app = express()
+
+//express and cors middleware
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/v1/movies", movies)
+app.use('*', (req,res)=>{
+    res.status(404).json({error: "not found"})
+})
+
+export default app 
