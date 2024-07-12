0b3aaef7607f31ca7718d83488435acb408a889b
aaron <aaronkornish.ak@gmail.com>
2024-07-12 15:56:25 -0700

    Imported MoviesController and used apiGetMovies in router get request

--- backend/api/movies.route.js
+++ backend/api/movies.route.js
@@ -1,8 +1,9 @@
 import express from 'express'
+import MoviesController from '../dao/movies.controller.js'
 
 //get access to express router
 const router = express.Router()
 
-router.route('/').get((req,res)=>res.send('hello world'))
+router.route('/').get(MoviesController.apiGetMovies)
 
 export default router 
