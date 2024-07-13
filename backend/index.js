d86f136bc7a20f706e3b5028fb1249579d8a7580
aaron <aaronkornish.ak@gmail.com>
2024-07-13 14:00:34 -0700

    Initialize ReviewsDAO by injecting the database client.

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import app from "./server.js";
 import mongodb from "mongodb"
 import dotenv from "dotenv"
 import MoviesDAO from "./dao/moviesDAO.js";
+import ReviewsDAO from "./dao/reviewsDAO.js";
 
 async function main() {
     dotenv.config()
@@ -15,6 +16,7 @@ async function main() {
         //connect to MongoDb cluster
         await client.connect()
         await MoviesDAO.injectDB(client)
+        await ReviewsDAO.injectDB(client)
 
         app.listen(port, () => {
             console.log(`server is running on port:${port}`)
