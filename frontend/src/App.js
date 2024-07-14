0f86c9bfc881175a11951e5b01e6cef59b0df2c3
aaron <aaronkornish.ak@gmail.com>
2024-07-13 21:30:41 -0700

    Created a header in app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import AddReview from "./components/add-review";
+import Login from "./components/login";
+import Movie from "./components/movie";
+import MoviesList from "./components/movies-list";
+
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+	return (
+		<div className="App">
+			<Navbar bg="light" expand="lg">
+				<Navbar.Brand href="#home">Movie Reviews</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="mr-auto">
+						<Nav.Link>
+              <Link to={"/movies"}>Movies</Link>
+            </Nav.Link>
+						<Nav.Link>
+              { user ? (
+              <a>Logout User</a>
+              ) : (
+              <Link to={"/login"}>Login</Link>
+              )}
+            </Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+		</div>
+	);
 }
 
 export default App;
